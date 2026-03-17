e5394088dc65e198b98e735c94318dfe2cb0d6da
Loola <natalyavinogradskaya@gmail.com>
2026-03-17 11:47:36 -0500

    Initial Commit

---
+++ datagigios/datagigiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  datagigiosApp.swift
+//  datagigios
+//
+//  Created by Nat on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct datagigiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ datagigiosTests/datagigiosTests.swift
@@ -0,0 +1,17 @@
+//
+//  datagigiosTests.swift
+//  datagigiosTests
+//
+//  Created by Nat on 3/17/26.
+//
+
+import Testing
+@testable import datagigios
+
+struct datagigiosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
